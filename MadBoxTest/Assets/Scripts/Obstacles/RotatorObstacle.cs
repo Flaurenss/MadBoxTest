344d2fad19edeee3a15b390c9bec13235ee99c10
Flaurenss <lorenzonapl@hotmail.com>
2020-03-30 21:07:50 +0200

    Modify obastacles velocity

--- MadBoxTest/Assets/Scripts/Obstacles/RotatorObstacle.cs
+++ MadBoxTest/Assets/Scripts/Obstacles/RotatorObstacle.cs
@@ -7,9 +7,8 @@ public class RotatorObstacle : GenericObstacle
     [SerializeField] private Vector3 rotation;
     public override void ObstacleBehaviour()
     {
-        // var rot = rotatorSpeed * Time.deltaTime;
         transform.Rotate(rotation.x * Time.deltaTime, 
-        rotation.y * Time.deltaTime
-        , rotation.z * Time.deltaTime);
+        rotation.y * Time.deltaTime,
+        rotation.z * Time.deltaTime);
     }
 }
